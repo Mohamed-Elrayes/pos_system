188f9320bf362f62da6965ee8731f140f983c625
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2024-10-12 18:51:52 +0300

    add init packages

---
+++ lib/warmup_app_provider.dart
@@ -0,0 +1,7 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'warmup_app_provider.g.dart';
+
+@riverpod
+Future<void> warmupApp(WarmupAppRef ref) {
+  return Future.value([]);
+}
---
+++ lib/warmup_app_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'warmup_app_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$warmupAppHash() => r'eb5b16c2ae1a091a1740e69fa7480f2a751e836b';
+
+/// See also [warmupApp].
+@ProviderFor(warmupApp)
+final warmupAppProvider = AutoDisposeProvider<void>.internal(
+  warmupApp,
+  name: r'warmupAppProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$warmupAppHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef WarmupAppRef = AutoDisposeProviderRef<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
